a587b375264eb9de2c19788ae34fa3907342d92c
Emoyee <mohadese.safari@ymail.com>
2021-08-14 12:03:42 +0430

    Model and service done

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import com.example.demo.exceptions.UserNotFoundException;
+import com.example.demo.model.Employee;
+import com.example.demo.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User with Id " + id + " was not found"));
+    }
+}
